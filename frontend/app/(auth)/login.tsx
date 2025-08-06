c5bf00de108116d354c6351aa8603782282840a7
TarzqnDev <mjjjantework@gmail.com>
2025-08-06 22:58:50 +0800

    Basic Auth Frontend Structure

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -1,13 +1,30 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function LoginScreen() {
     return (
         <View>
-            <Text>Login Screen</Text>
-            <Link href="/(auth)/register" asChild>
-                <Text>Go to Register</Text>
-            </Link>
+
+            <View>
+                <Text>Login Screen</Text>
+            </View>
+
+            <View>
+                <Text>Email Address</Text>
+                <TextInput placeholder="Email"/>
+
+                <Text>Password</Text>
+                <TextInput placeholder="Password" secureTextEntry/>
+
+                <Button title="Login" onPress={() => {}}/>
+            </View>
+
+            <View>
+                <Link href="/(auth)/register" asChild>
+                    <Text>Go to Register</Text>
+                </Link>
+            </View>
+            
         </View>
     );
 }
--- frontend/app/(auth)/register.tsx
+++ frontend/app/(auth)/register.tsx
@@ -1,7 +1,30 @@
-import { Text } from "react-native";
+import { Link } from "expo-router";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function RegisterScreen() {
     return (
-        <Text>Register Screen</Text>
+        <View>
+
+            <View>
+                <Text>Register Screen</Text>
+            </View>
+
+            <View>
+                <Text>Email Address</Text>
+                <TextInput placeholder="Email" />
+
+                <Text>Password</Text>
+                <TextInput placeholder="Password" secureTextEntry/>
+
+                <Button title="Register" onPress={() => {}}/>               
+            </View>
+
+            <View>
+                <Link href="/(auth)/login" asChild>
+                    <Text>Go to Login</Text>
+                </Link>
+            </View>
+            
+        </View>
     );
 }
